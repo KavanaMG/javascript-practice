a6b0690edfe27a77feffb4bf7e7e49b77a29fb0d
KavanaMG <32697564+KavanaMG@users.noreply.github.com>
2021-10-05 16:27:02 +0200

    IIFE

---
+++ functions/C2/script.js
@@ -0,0 +1,10 @@
+/*Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!*/
+
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+  
+    document.querySelector('body').addEventListener('click', function () {
+      header.style.color = 'blue';
+    });
+  })();
