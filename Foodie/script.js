581cae35d1bf855b92447bd632f0698b11ef134a
KavanaMG <32697564+KavanaMG@users.noreply.github.com>
2021-11-23 21:44:11 +0100

    Implemented Home and Dish section

--- Foodie/script.js
+++ Foodie/script.js
@@ -5,3 +5,31 @@ menu.onclick = () =>{
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 }
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+  
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 150,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+});
+
